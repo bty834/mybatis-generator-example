53a1000d266bbc69391e3c5f0b957a550671247c
wuxueyan <wuxueyan@pinduoduo.com>
2024-08-14 10:04:13 +0800

    add plugin dev module

---
+++ mybatis-plugin-dev/src/test/java/io/github/bty834/mybatisplugindev/MybatisPluginDevApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bty834.mybatisplugindev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPluginDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
